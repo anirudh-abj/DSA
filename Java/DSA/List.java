8085e71813e29d59e5a2978dacb119639e1508b4
anirudh-abj <anirudh.abj9@gmail.com>
2025-04-05 17:52:39 +0530

    bug fixes and performance improvements in List.java

--- Java/DSA/List.java
+++ Java/DSA/List.java
@@ -4,28 +4,38 @@ public class List<T>{
 	protected int size, capacity;
 	private T[] arr;
 
+	@SuppressWarnings("unchecked")
 	public List(int maxCapacity){
+		if(maxCapacity <= 0){
+			throw new IndexOutOfBoundsException("Capacity can't be negitive or zero");
+		}
 		this.arr = (T[]) new Object[maxCapacity];
 		this.size = 0;
 		this.capacity = maxCapacity;
 	}
 
+	@SuppressWarnings("unchecked")
 	public List(){
 		this.arr = (T[]) new Object[5];
 		this.size = 0;
 		this.capacity = 5;
 	}
 
+	@SuppressWarnings("unchecked")
 	public void resize(){
 		this.capacity += this.capacity;
 		T[] newArr = (T[]) new Object[this.capacity];
-		for(int i = 0; i < this.size; i++){
-			newArr[i] = this.arr[i];
-		}
+		// for(int i = 0; i < this.size; i++){
+		// 	newArr[i] = this.arr[i];
+		// }
+
+		System.arraycopy(this.arr, 0, newArr, 0, this.size);
 
 		this.arr = newArr;
 	}
 
+
+	@SuppressWarnings("unchecked")
 	public void shrinkToFit(){
 		this.capacity = this.size;
 		T[] newArr = (T[]) new Object[this.capacity];
@@ -54,25 +64,24 @@ public class List<T>{
 			this.resize();
 		}
 
-		T temp = newElement;
-		T tmp;
-		for(int i = pos; i <= this.size; i++){
-			tmp = this.arr[i];
-			this.arr[i] = temp;
-			temp = tmp;
+		for(int i = this.size; i > pos; i--){
+			this.arr[i] = this.arr[i - 1];
 		}
 
+		this.arr[pos] = newElement;
 		this.size += 1;
 	}
 
 	public void deleteByPos(int pos, boolean shrink){
-		if(pos > this.size || pos < this.size){
+		if(pos < 0 || pos > this.size){
 			throw new IndexOutOfBoundsException("Index : "+ String.valueOf(pos) + "; SIZE : " + String.valueOf(this.size));
 		}
 
 		for(int i = pos; i < this.size - 1; i++){
 			this.arr[i] = this.arr[i + 1];
 		}
+
+		this.arr[this.size - 1] = null;
 
 		this.size -= 1;
 
@@ -82,38 +91,70 @@ public class List<T>{
 	}
 
 	public void deleteByVal(T val, boolean deleteAllInstances, boolean shrink){
-		boolean valFound = false;
-		for(int i = 0; i < this.size; i++){
-			if(this.arr[i] == val){
+		boolean valFound = false, firstDelete = false;
+		// for(int i = 0; i < this.size; i++){
+		// 	if(java.util.Objects.equals(this.arr[i],  val)){
+		// 		valFound = true;
+		// 		for(int j = i; j < this.size - 1; j++){
+		// 			this.arr[j] = this.arr[j + 1];
+		// 		}
+
+		// 		this.arr[this.size - 1] = null;
+		// 		this.size -= 1;
+
+		// 		if (!deleteAllInstances) {
+		// 			break;
+		// 		}
+
+		// 		else{
+		// 			i--;
+		// 		}
+
+		// 	}
+		// }
+
+		int readIndex = 0, writeIndex = 0;
+		while(readIndex < this.size){
+			if(this.arr[readIndex] == val && (deleteAllInstances || !firstDelete)){
 				valFound = true;
-				for(int j = i; j < this.size - 1; j++){
-					this.arr[j] = this.arr[j + 1];
+				if(!deleteAllInstances){
+					firstDelete = true;
 				}
 
-				this.size -= 1;
-
-				if (!deleteAllInstances) {
-					break;
-				}
-
-				else{
-					i = 0;
-				}
-
+				readIndex++;
+				continue;				
 			}
-		}
 
-		if (!valFound){
-			System.out.println("Value not present in the list.");
+			this.arr[writeIndex] = this.arr[readIndex];
+			writeIndex++;
+			readIndex++;
 		}
 
 		if(shrink){
 			this.shrinkToFit();
 		}
+
+		if (!valFound){
+			System.out.println("Value not present in the list.");
+			return;
+		}
+
+		this.size = writeIndex;
 	}
 
 	public T get(int index){
+		if(index < 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Index : "+ String.valueOf(index) + "; SIZE : " + String.valueOf(this.size));
+		}
 		return this.arr[index];
+	}
+
+	public void set(int index, T val){
+		if(index < 0 || index > this.size){
+			throw new IndexOutOfBoundsException("Index : "+ String.valueOf(index) + "; SIZE : " + String.valueOf(this.size));
+		}
+
+		this.arr[index] = val;
 	}
 
 	public int size(){
